dd8bdfc9cffd5e53d55d0b4bc5a51ed920b3f35a
Brandon Wilcox <itsmrasync@gmail.com>
2020-02-12 20:38:43 -0500

    Setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# SandCastle-Simulator
- 
---
+++ src/Server/Modules/Controllers/PlayerLoader.lua
@@ -0,0 +1,41 @@
+-- Player Loader
+-- MrAsync
+-- February 12, 2020
+
+
+
+local PlayerLoader = {}
+self = PlayerLoader
+
+--//Services
+
+--//Controllers
+local dataStore2
+
+--//Classes
+
+--//Data
+
+
+function PlayerLoader:Start()
+
+
+
+
+end
+
+
+function PlayerLoader:Init()
+    --//Services
+
+    --//Controllers
+    dataStore2 = self.Modules.DataStore2
+
+    --//Classes
+
+    --//Data
+
+end
+
+
+return PlayerLoader
